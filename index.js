3f8f2b5fdaf594e8802d25236872fc483a9f98a7
Grant Ours <grant.ours55@gmail.com>
2022-07-21 16:24:22 -0600

    commit tree 7bfb4b98b87c01ee97a62747c8cfdca2b81bc165

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// })
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (object) {
+//     console.log(object);
+// })
+// .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+// });
+
+function submitData(username, useremail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: username,
+            email: useremail,
+        }) ,
+    })
+    .then((res) => res.json())
+    .then((data) => document.body.append(data.id))
+    .catch(function (error) {;
+        document.body.append(error);
+    });
+}
+submitData("steve", "steve@gmail.com")
